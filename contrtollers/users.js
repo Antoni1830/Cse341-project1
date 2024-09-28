bc4355c8a08852bcc15ed033e4e98f09290a677e
Antoni1830 <jemmotte08@gmail.com>
2024-09-28 13:46:40 -0400

    installed swagger

--- contrtollers/users.js
+++ contrtollers/users.js
@@ -3,6 +3,7 @@ const mongodb= require('../data/database');
 const ObjectId =require('mongodb').ObjectId;
 
 const getAll= async(req,res)=>{
+  //#swagger.tags=['contacs']
  const result= await mongodb.getDatabase().db().collection('contacs').find();
  result.toArray(req).then((users)=>{
     res.setHeader('Contet-Type','aplication/json');
@@ -10,14 +11,73 @@ const getAll= async(req,res)=>{
  });
  };
 const getSingle =async(req,res)=>{
+    //#swagger.tags=['contacs']
     const userId=new ObjectId(req.params.id);
     const result= await mongodb.getDatabase().db().collection('contacs').find({id: userId});
-    result=toArray().then((users)=>{
+    result.toArray().then((users)=>{
        res.setHeader('Contet-Type','aplication/json');
        res.status(200).json(users[0])
     });
+
 };
+
+const createUser= async(req,res) =>{  
+    //#swagger.tags=['contacs']
+    const user={
+    firstName:req.body.firstName,
+    lastName:req.body.lastName,
+    email:req.body.email,  
+    favoriteColor:req.body.favoriteColor,
+    birthday:req.body.birthday
+};
+const response= await mongodb.getDatabase().db().collection('contacs').insertOne(user);
+     if (response.acknowledged){
+     res.status(204).send();
+    } else{
+      res.status(500).json(response.error||'algo malo papi');
+    
+    }
+
+};
+
+const updateUser= async(req,res)=>async(req,res) =>{  
+    //#swagger.tags=['contacs']
+    const userId=new ObjectId(req.params.id);
+    const user={
+    username:req.body.username,
+    email:req.body.email,
+    name:req.body.name,  
+    ipaddress:req.body.ipaddress,
+};
+const response= await mongodb.getDatabase().db().collection('contacs').replaceOne({id: userId}, user);
+     if (response.modifiedCount>0){
+     res.status(204).send();
+    } else{
+      res.status(500).json(response.error||'algo malo papi');
+    
+    }
+
+};
+
+const deleteUser = async (req, res) => {
+    //#swagger.tags=['contacs']
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDatabase().db().collection('contacs').deleteOne({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(200).json({
+      response: response,
+      message: "Deleted contact successfully.",
+    });
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
 module.exports={
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const router= require('express').Router();
 
+router.use('/',require('./sawgger'));
+
+router.get('/',(req,res)=>{
+    //#swagger.tags=['hello world']
+    res.send('I love you <3');
+});
 //router.get('/',(req,res)=>{res.send('qlq el mio')});
 
 router.use('/users',require('./users'))
